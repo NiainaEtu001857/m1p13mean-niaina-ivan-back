f48658bbc9e4dfcc48f124cd58b77b90e0a95578
IvanETU001597 <ivantahiana@gmail.com>
2026-02-17 08:36:00 +0300

    feat: list and add stock

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-require("dotenv").config();
+require("dotenv").config({ quiet: true });
 require("./config/db")();
 
 
