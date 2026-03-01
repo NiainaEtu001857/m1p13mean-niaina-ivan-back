d365bf365bd0f9aab3be71025eff750b5ecd536c
IvanETU001597 <ivantahiana@gmail>
2026-03-01 10:21:12 +0300

    call list

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -2,13 +2,39 @@ const Client = require("../models/Client");
 const Shop = require("../models/Shop");
 const Service = require("../models/Service");
 
-
 exports.getStats = async (req, res) =>{
     try{
         const nbrClient = await Client.countDocuments();
         const nbrShop = await Shop.countDocuments();
         const nbrService= await Service.countDocuments();
         res.status(200).json({ nbrClient , nbrShop, nbrService });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+exports.getAllClients = async (req, res) => {
+    try {
+        const clients = await Client.find().select("-password -role");
+        res.status(200).json(clients);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+exports.getAllShops = async (req, res) => {
+    try {
+        const shops = await Shop.find().select("-password -role");
+        res.status(200).json(shops);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+exports.getAllServices = async (req, res) => {
+    try {
+        const services = await Service.find();
+        res.status(200).json(services);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const { AdminLogin, } = require("../controllers/auth.controllers");
 const { getStats } = require("../controllers/admin.controllers");
 const { authMiddleware, requireRole } = require("../midllewares/auth");
+const { getAllClients, getAllShops, getAllServices } = require("../controllers/admin.controllers");
 
 router.post("/login", AdminLogin );
 router.get("/stats", authMiddleware, requireRole("ADMIN"), getStats);
+router.get("/clients", authMiddleware, requireRole("ADMIN"), getAllClients);
+router.get("/shops", authMiddleware, requireRole("ADMIN"), getAllShops);
+router.get("/services", authMiddleware, requireRole("ADMIN"), getAllServices);
 module.exports = router;
